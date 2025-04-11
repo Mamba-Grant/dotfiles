1f82e9f155db2e3b75ea724760562a226f1d5e1d
installer <installer@gmail.com>
2025-04-11 09:58:46 -0500

    added thunar

--- hosts/mambaLaptop/packages-fonts.nix
+++ hosts/mambaLaptop/packages-fonts.nix
@@ -159,15 +159,15 @@ in {
         git.enable = true;
         nm-applet.indicator = true;
 
-        # thunar.enable = true;
-        # thunar.plugins = with pkgs.xfce; [
-        #     exo
-        #     mousepad
-        #     thunar-archive-plugin
-        #     thunar-dropbox-plugin
-        #     thunar-volman
-        #     tumbler
-        # ];
+        thunar.enable = true;
+        thunar.plugins = with pkgs.xfce; [
+            exo
+            mousepad
+            thunar-archive-plugin
+            thunar-dropbox-plugin
+            thunar-volman
+            tumbler
+        ];
 
         #steam = {
         #  enable = true;
